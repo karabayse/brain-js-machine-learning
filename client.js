4ebae6c89aedce46ca764abda79f21cb59c6ded0
Kara Bayse <karabayse@gmail.com>
2018-08-31 08:57:37 -0500

    added training data

---
+++ client.js
@@ -0,0 +1,11 @@
+const brain = require('brain.js');
+const network = new brain.NeuralNetwork();
+
+// Training Data
+network.train([
+  { input: [0,0,0], output: [0] },
+  { input: [0,0,1], output: [0] },
+  { input: [0,1,1], output: [0] },
+  { input: [1,0,1], output: [1] },
+  { input: [1,1,1], output: [1] }
+]);
