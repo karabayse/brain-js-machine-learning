a217156eb56dfc2598e51f25e3829e18499fba62
Kara Bayse <karabayse@gmail.com>
2018-09-07 21:34:06 -0500

    tested text against training data

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 // Requires
 const brain = require('brain.js');
-const data = require('data.json');
+const data = require('./data.json');
 
 // Long Short-Term Memory
 const network = new brain.recurrent.LSTM();
@@ -10,3 +10,11 @@ const trainingData = data.map(item => ({
   input: item.text,
   output: item.category
 }));
+
+network.train(trainingData, {
+  iterations: 2000
+});
+
+// Run a piece of text against training data
+const output = network.run('I fixed the power supply');
+console.log(`Category: ${output}`);
